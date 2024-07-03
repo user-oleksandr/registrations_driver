901181300ad81357ffba3816c4ded5821f9e102f
user-oleksandr <name.oleksandr@gmail.com>
2024-07-03 13:25:39 +0300

    add changes

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -11,15 +11,17 @@ function RegistrationForm() {
   const authToken = process.env.REACT_APP_AUTH_TOKEN;
   
   const [driverInfo, setDriverInfo] = useState('');
-  const [driverLicense, setDriverLicense] = useState('');
-  const [autoInfo, setAutoInfo] = useState('');
+  const [nameLat, setNameLat] = useState('');
   const [contactInfo, setContactInfo] = useState('');
-
+  const [driverPassport, setDriverPassport] = useState('');
+  const [driverLicense, setDriverLicense] = useState('');
+  
   const [driverInfoError, setDriverInfoError] = useState(false);
-  const [driverLicenseError, setDriverLicenseError] = useState(false);
-  const [autoInfoError, setAutoInfoError] = useState(false);
+  const [nameLatError, setNameLatError] = useState(false);
   const [contactInfoError, setContactInfoError] = useState(false);
-
+  const [driverPassportError, setDriverPassportError] = useState(false);
+  const [driverLicenseError, setDriverLicenseError] = useState(false);
+ 
   const [isRegistered, setIsRegistered] = useState(false);
   
   const handleChangeDriverInfo = (e) => {
@@ -33,28 +35,36 @@ function RegistrationForm() {
     setDriverInfoError(false);
   };
 
-   const handleChangeDriverLicense = (e) => {
-    setDriverLicense(e.target.value);
-    setDriverLicenseError(false);
-  };
+  const handleChangeNameLat = (e) => {
+    const inputValue = e.target.value;
+    const formattedValue = inputValue
+      .split(' ') 
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) 
+      .join(' '); 
 
-  const handleChangeAutoInfo = (e) => {
-    setAutoInfo(e.target.value);
-    setAutoInfoError(false);
+    setNameLat(formattedValue);
+    setNameLatError(false);
   };
 
   const handleChangeContactInfo = (e) => {
     let inputValue = e.target.value;
-    // Ограничиваем ввод только цифрами
     inputValue = inputValue.replace(/\D/g, '');
-    // Ограничиваем длину ввода до 10 цифр
     if (inputValue.length > 10) {
       inputValue = inputValue.slice(0, 10);
     }
-    // Форматируем номер телефона
     const formattedPhoneNumber = formatPhoneNumber(inputValue);
     setContactInfo(formattedPhoneNumber);
     setContactInfoError(false);
+  };
+
+  const handleChangeDriverPassport = (e) => {
+    setDriverPassport(e.target.value.toUpperCase());
+    setDriverPassportError(false);
+  };
+
+  const handleChangeDriverLicense = (e) => {
+    setDriverLicense(e.target.value);
+    setDriverLicenseError(false);
   };
 
   const handleSubmit = async (e) => {
@@ -63,44 +73,49 @@ function RegistrationForm() {
     if (!driverInfo.trim()) {
       setDriverInfoError(true);
     }
-    if (!driverLicense.trim()) {
-      setDriverLicenseError(true);
-    }
-    if (!autoInfo.trim()) {
-      setAutoInfoError(true);
+    if (!nameLat.trim()) {
+      setNameLatError(true);
     }
     if (!contactInfo.trim()) {
       setContactInfoError(true);
     }
-
-    if (!driverInfo.trim() || !driverLicense.trim() || !autoInfo.trim() || !contactInfo.trim()) {
+    if (!driverPassport.trim()) {
+      setDriverPassportError(true);
+    }
+    if (!driverLicense.trim()) {
+      setDriverLicenseError(true);
+    }
+  
+    if (!driverInfo.trim() || !nameLat.trim() || !contactInfo.trim() || !driverPassport.trim() || !driverLicense.trim()) {
       alert('Будь ласка, заповніть усі поля');
       return;
     }
 
     try {
-      const data = `${driverInfo}, ${driverLicense.toUpperCase()}, ${autoInfo.toUpperCase()}, ${contactInfo},`;
+      const data = `${driverInfo}, ${nameLat}, ${contactInfo}, ${driverPassport}, ${driverLicense.toUpperCase()}`;
       const response = await axios.post(apiRegistrations, data, {
         headers: {
-          'Authorization': `Basic ${authToken}`,
-          'Content-Type': 'application/json'
+          // 'Authorization': `Basic ${authToken}`,
+          // 'Content-Type': 'application/json'
+          'Content-Type': 'text/plain; charset=utf-8',
+          'Authorization': 'Basic ' + btoa('admin:1234')
         }    
       });
       
-      console.log(driverInfo, driverLicense, autoInfo, contactInfo);
+      console.log(driverInfo, nameLat, contactInfo, driverPassport, driverLicense);
       alert('Ви успішно зареєструвались! Щасливої дороги!!!');
 
       setDriverInfo('');
-      setDriverLicense('');
-      setAutoInfo('');
+      setNameLat('');
       setContactInfo('');
+      setDriverPassport('');
+      setDriverLicense('');
       setIsRegistered(true);
     } catch (error) {
       console.error('Помилка при відправленні даних:', error);
     }
   };
 
-  const upperCaseAutoInfo = autoInfo.toUpperCase();
   const upperCaseDriverLicense = driverLicense.toUpperCase();
 
   return (
@@ -119,19 +134,10 @@ function RegistrationForm() {
         <label className="label">
           <input
             type="text"
-            value={upperCaseDriverLicense}
-            onChange={handleChangeDriverLicense}
-            className={`input ${driverLicenseError ? 'error' : ''}`}
-            placeholder="Водійське посвідчення"
-          />
-        </label>
-        <label className="label">
-          <input
-            type="text"
-            value={upperCaseAutoInfo}
-            onChange={handleChangeAutoInfo}
-            className={`input ${autoInfoError ? 'error' : ''}`}
-            placeholder="Номерний знак авто"
+            value={nameLat}
+            onChange={handleChangeNameLat}
+            className={`input ${nameLatError ? 'error' : ''}`}
+            placeholder="Name(lat.)"
           />
         </label>
         <label className="label">
@@ -143,6 +149,25 @@ function RegistrationForm() {
             placeholder="Контактний телефон"
           />
         </label>
+        <label className="label">
+          <input
+            type="text"
+            value={driverPassport}
+            onChange={handleChangeDriverPassport}
+            className={`input ${driverPassportError ? 'error' : ''}`}
+            placeholder="Паспорт водія"
+          />
+        </label>
+        <label className="label">
+          <input
+            type="text"
+            value={upperCaseDriverLicense}
+            onChange={handleChangeDriverLicense}
+            className={`input ${driverLicenseError ? 'error' : ''}`}
+            placeholder="Водійське посвідчення"
+          />
+        </label>
+    
         <button type="submit" className="button">Зареєструватись</button>
       </form>
       {/* {isRegistered && <Animations />} */}
